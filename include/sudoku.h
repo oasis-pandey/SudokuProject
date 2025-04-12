c0994f827464726425bd17f3bbb113d923a19536
bidhan7s <beingbidhan@gmail.com>
2025-04-11 21:09:22 -0500

    solveBoardEfficient docs

--- include/sudoku.h
+++ include/sudoku.h
@@ -43,8 +43,30 @@ std::tuple<int, int, int> findNextCell(int** BOARD);
 
 
 /**
-  * TODO: Provide appropriate Documentation, see other examples provided within the projects
-  */
+ * @brief Efficiently solves a Sudoku board using the Minimum Remaining Value (MRV) heuristic.
+ * 
+ * This function implements an optimized Sudoku solving algorithm that uses the MRV heuristic
+ * to reduce the branching factor in the backtracking search. Instead of filling cells in a
+ * fixed order (row by row, column by column), it selects the empty cell with the fewest
+ * valid number options at each step.
+ * 
+ * The MRV heuristic significantly improves performance by:
+ * 1. Reducing the number of backtracks needed to find a solution
+ * 2. Prioritizing cells with fewer options, which are more likely to lead to a solution
+ * 3. Providing early exit opportunities when cells with only one valid option are found
+ * 
+ * The function uses recursive backtracking with the following steps:
+ * 1. Find the next cell to fill using the MRV heuristic via findNextCell()
+ * 2. If no empty cells remain, the board is solved
+ * 3. Try placing numbers 1-9 in the selected cell
+ * 4. For each valid number, recursively attempt to solve the rest of the board
+ * 5. If a solution is found, return true
+ * 6. If no solution is found, backtrack by removing the number and try the next option
+ * 7. If no valid number works, return false to trigger further backtracking
+ * 
+ * @param BOARD A pointer to the 9x9 Sudoku board to be solved
+ * @return true if the board is successfully solved, false otherwise
+ */
 bool solveBoardEfficient(int** BOARD);
 
 /**
