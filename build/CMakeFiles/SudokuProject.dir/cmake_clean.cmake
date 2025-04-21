dff7e83a95a0ab16ad5a9594f77344799a0b5b38
Oasis Pandey <oasis@Oasiss-MacBook-Air.local>
2025-04-21 16:12:57 -0500

    minor adjustments

---
+++ build/CMakeFiles/SudokuProject.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SudokuProject.dir/main.cpp.o"
+  "CMakeFiles/SudokuProject.dir/main.cpp.o.d"
+  "CMakeFiles/SudokuProject.dir/src/generator.cpp.o"
+  "CMakeFiles/SudokuProject.dir/src/generator.cpp.o.d"
+  "CMakeFiles/SudokuProject.dir/src/sudoku.cpp.o"
+  "CMakeFiles/SudokuProject.dir/src/sudoku.cpp.o.d"
+  "CMakeFiles/SudokuProject.dir/src/sudoku_io.cpp.o"
+  "CMakeFiles/SudokuProject.dir/src/sudoku_io.cpp.o.d"
+  "CMakeFiles/SudokuProject.dir/src/utils.cpp.o"
+  "CMakeFiles/SudokuProject.dir/src/utils.cpp.o.d"
+  "SudokuProject"
+  "SudokuProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SudokuProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/SudokuProject.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SudokuProject.
+# This may be replaced when dependencies are built.
